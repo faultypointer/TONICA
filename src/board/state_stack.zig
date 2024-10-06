d83e346f295cc3a7a1f392fb6125c08f7b3f2007
faultypointer <faultypointer@proton.me>
2024-10-06 07:17:47 +0545

    stack of previous states

---
+++ src/board/state_stack.zig
@@ -0,0 +1,20 @@
+const Bstate = @import("../board.zig").BState;
+
+pub const BSTATE_STACK_SIZE: usize = 1024;
+
+// a stack to hold the previous board states
+// the number of elements in the can be directly accessed through the states feild's len property
+pub const StateStack = struct {
+    states: [BSTATE_STACK_SIZE]Bstate = undefined,
+    top: usize = 0,
+
+    pub fn push(self: *StateStack, state: Bstate) void {
+        self.states[self.top] = state;
+        self.top += 1;
+    }
+
+    pub fn pop(self: *StateStack) void {
+        self.top -= 1;
+        return self.states[self.top];
+    }
+};
