a3e989a48fa35218b783de982d3735b0d0595d3d
faultypointer <faultypointer@proton.me>
2024-10-06 21:06:27 +0545

    remove LS1B from bitboard

--- src/board/bitboard.zig
+++ src/board/bitboard.zig
@@ -1,11 +1,20 @@
+const std = @import("std");
+const math = std.math;
 pub const BitBoard = u64;
 
-// pub const BitBoard = struct {
-//     board: u64,
-//
-//     pub fn init(board: u64) BitBoard {
-//         return .{
-//             .board = board,
-//         };
-//     }
-// };
+const MAX64 = 0xffffffffffffffff;
+
+// removes the least significat 1 Bit from the bitboard and returns it position
+pub fn removeLS1B(bb: *BitBoard) u6 {
+    std.debug.assert(bb.* != 0);
+    const two = MAX64 - bb.* + 1;
+    const loc = math.log2_int(u64, bb.* & two);
+    bb.* &= bb.* - 1;
+    return loc;
+}
+
+test "removesLSB1" {
+    var a: u64 = 0b100;
+    try std.testing.expectEqual(2, removeLS1B(&a));
+    try std.testing.expectEqual(0, a);
+}
