d56ed26e232af62d3055e4e51957825990a2c872
faultypointer <faultypointer@proton.me>
2024-10-13 12:26:15 +0545

    testing unmake

--- src/main.zig
+++ src/main.zig
@@ -12,7 +12,7 @@ const PieceType = @import("board/types.zig").PieceType;
 pub fn main() !void {
     std.debug.print("Qf7#\n", .{});
 
-    const board = Board.readFromFen("rnbqk2r/pppp2pp/5n2/2b1pp2/2B1P3/5Q2/PPPPNPPP/RNB2RK1 b kq - 0 1");
+    var board = Board.readFromFen("rnbqk2r/pppp2pp/5n2/2b1pp1Q/2B1P3/8/PPPPNBPP/RN3RK1 b kq - 0 1");
     var movgen = MovGen.init();
     // std.debug.print("Starting board\n", .{});
     // printBitboard(board.side_bb[0] | board.side_bb[1]);
@@ -25,15 +25,16 @@ pub fn main() !void {
     // std.debug.print("board after h5\n", .{});
     // printBitboard(board.side_bb[0] | board.side_bb[1]);
     std.debug.print("possible attack for {any} now\n", .{board.state.turn});
-    const moves = movgen.generateMoves(board);
+    const moves = movgen.generateMoves(&board);
     for (0..moves.len) |i| {
         std.debug.print("==========================================================================\n", .{});
         // std.debug.print("{b:0>64}\n", .{moves.moves[i].data});
-        var temp_board = board;
-        temp_board.makeMove(moves.moves[i]);
+        board.makeMove(moves.moves[i]);
         // printBitboard(board.side_bb[0] | board.side_bb[1]);
-        temp_board.printBoard();
-        temp_board.state.print();
+        board.printBoard();
+        board.state.print();
+        std.debug.print("Is King in check: {any}\n", .{movgen.isInCheck(&board, board.state.turn.opponent())});
         _ = std.io.getStdIn().reader().readByte() catch unreachable;
+        board.unMakeMove();
     }
 }
